1e7a0e209f915d8eda1ec1c8714e26c87e1b5ffa
Srinath <srinathpanuganti@gmail.com>
2025-08-20 07:35:43 +0530

    exploring validator

--- src/app.js
+++ src/app.js
@@ -75,13 +75,19 @@ app.patch("/user", async (req, res) => {
     const userId = req.body.userId;
     const data = req.body;
     try{
+        const ALLOWED_UPDATES = ["firstName", "age", "gender", "skills","password", "photoUrl", "userId"];
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));    
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+
         await User.findByIdAndUpdate({ _id: userId }, data, {
             runValidators: true,
         });
         res.send("User updated successfully");
     }
     catch(err){
-        res.status(400).send("Error updating" + err.message);
+        res.status(400).send("Error updating " + err.message);
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator =  require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         unique: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email Id");
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter strong password");
+            }
+        }
     },
     age: {
         type: Number,
@@ -38,6 +49,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://image.png", 
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL");
+            }
+        }
     },
     skills: {
         type: [String],

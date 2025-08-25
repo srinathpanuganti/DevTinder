6101d5f12bb7514509b55558cb7b0c4abf09340f
Srinath <srinathpanuganti@gmail.com>
2025-08-25 19:57:34 +0530

    Feed API

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
-const USER_SAFE_DATA = "firstName lastName age gender";
+const USER_SAFE_DATA = "firstName lastName age gender skills photoUrl";
 
 //get all the connection requests for all the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -49,5 +50,42 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         res.status(400).send({ message: err.message });
     }
 });
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        limit = limit>50 ? 50: limit;
+        const skip = (page-1)*limit;
+
+        //Find all connection requests (sent+received)
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id },
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) =>{
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } },
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.json({ data: users });
+    }
+    catch(err){
+        res.status(400).send({ message: err.message });
+    }
+})
 
 module.exports = userRouter;
